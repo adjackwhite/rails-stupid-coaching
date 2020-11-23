520ac292772af1c7307a71a84958b3b33ca46850
Adam White <adjackwhite@gmail.com>
2020-11-23 14:45:40 +0800

    app

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,12 @@
+class AnswersController < ApplicationController
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @reply = "Great!"
+    elsif @question.include?('?')
+      @reply = "Silly question, get dressed and go to work!"
+    else
+      @reply = "I don't care, get dressed and go to work!"
+    end
+  end
+end
